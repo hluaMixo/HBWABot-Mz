56079fa4ee9bae71fcd6d7e2774d4d646dd6617b
hluaMixo <162483891+hluaMixo@users.noreply.github.com>
2024-03-07 10:26:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
-global.GroupWcMsg = true // false a i dah chuan welcome message i off ang
+global.AntiCallBot = false // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
+global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.available = true // false a i dah chuan offline angin a awm ang
@@ -37,12 +37,12 @@ global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile
 //messages
 global.mess = {
     success: '\n*©HBWABot*!', 
-    admin: 'He features hi chu admin-te chauh hman thei tur a siam a ni!',
-    botAdmin: 'Admin-ah min dah hmasa phawt la chuan ka ti thei ang!',
-    owner: 'He features hi a neitu chauh hman thei tura siam a ni!',
+    admin: '🙄😂',
+    botAdmin: '😀',
+    owner: '😀',
     group: 'Hei chu group-ah chauh hman thei tura siam a ni!',
-    private: 'Hei chu private chat-ah chauh hman thei tura siam a ni!',
-    bot: 'Hei chu kei mah chauh hman thei tur a siam a ni',
+    private: '😀',
+    bot: 'i',
 }
 
 let file = require.resolve(__filename)
